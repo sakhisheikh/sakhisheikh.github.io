044a0dc478b6eb82922112a458344c4d9c7f9c33
sakhisheikh <sakhi.mansoor@venturedive.com>
2018-11-28 03:19:37 +0500

    Updates

---
+++ component---src-pages-404-js-bd6dea29c0aed076f554.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{245:function(e,n,t){"use strict";t.r(n);var a=t(0),r=t.n(a),c=t(291);n.default=function(){return r.a.createElement("div",{style:{color:"red"}},r.a.createElement(c.a,{headerText:"N0 page found"}))}},291:function(e,n,t){"use strict";var a=t(0),r=t.n(a);n.a=function(e){var n=e.headerText;return r.a.createElement("h1",null,n)}}}]);
+//# sourceMappingURL=component---src-pages-404-js-bd6dea29c0aed076f554.js.map
